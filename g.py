1e405117a88cb1f2e0fb3e6283f6910f85f50169
Jayakumar01murugan <109781138+Jayakumar01murugan@users.noreply.github.com>
2022-09-08 16:01:22 +0530

    python pgm

---
+++ g.py
@@ -0,0 +1,12 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
